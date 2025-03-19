e624c121d3521ccb09592c7356df14775d23377e
NeuroGirl <vesus0206@gmail.com>
2025-03-19 04:58:01 +0300

    Printouts appearance changes

--- lab_2/run_code_script.py
+++ lab_2/run_code_script.py
@@ -3,11 +3,11 @@ import os
 
 
 def run_1_lab_tasks():
-    "Функция для запуска заданий 1 лабы"
+    "Функция для запуска заданий 2 лабы"
     tasks = ['1', '2', '3', '5']
     for i in tasks:
         print(' ')
-        print('######## Lab_1', f'Task_{i}', '###########################################################')
+        print('######## Lab_2', f'Task_{i}', '###########################################################')
         PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'tasks', f'task{i}', f'task{i}.py'))
         subprocess.run("python " + PATH, shell=True)
 
