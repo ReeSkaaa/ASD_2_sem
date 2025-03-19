54d8b0f3c2bf4738744adb26426ae9485258f9fc
Your Name <katyast0505@gmail.com>
2025-03-19 11:07:39 +0300

    update task 5 lab 4

---
+++ lab_4/tasks/task5/task5.py
@@ -0,0 +1,17 @@
+def prefix_function(s):
+    n = len(s)
+    pi = [0] * n  # Инициализация массива префиксной функции
+    for i in range(1, n):  # Начинаем с 1, так как pi[0] всегда 0
+        j = pi[i - 1]  # Берем значение префиксной функции для предыдущего символа
+        while j > 0 and s[i] != s[j]:  # Пока j > 0 и символы не совпадают
+            j = pi[j - 1]  # Уменьшаем j
+        if s[i] == s[j]:  # Если символы совпали
+            j += 1  # Увеличиваем j
+        pi[i] = j  # Записываем значение префиксной функции
+    return pi
+
+with open("input.txt", "r") as f:
+    s = f.readline().strip()
+    result = prefix_function(s)
+with open("output.txt", "w") as f:
+    f.write(" ".join(map(str, result)))
---
+++ lab_4/tests/task5.py
@@ -0,0 +1,36 @@
+import sys
+import time
+def prefix_function(s):
+    n = len(s)
+    pi = [0] * n  # Инициализация массива префиксной функции
+    for i in range(1, n):  # Начинаем с 1, так как pi[0] всегда 0
+        j = pi[i - 1]  # Берем значение префиксной функции для предыдущего символа
+        while j > 0 and s[i] != s[j]:  # Пока j > 0 и символы не совпадают
+            j = pi[j - 1]  # Уменьшаем j
+        if s[i] == s[j]:  # Если символы совпали
+            j += 1  # Увеличиваем j
+        pi[i] = j  # Записываем значение префиксной функции
+    return pi
+
+
+# Тестовые данные
+test_n = 5
+test_edges = [(1, 2), (2, 3), (3, 1), (3, 4), (4, 5)]
+
+# Начало отсчета времени
+start_time = time.perf_counter()
+
+answer = prefix_function('aaaAAA')
+
+# Конец отсчета времени
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+
+total_size = sys.getsizeof(answer)
+for item in answer:
+    total_size += sys.getsizeof(item)
+
+
+print(f"Ответ: {answer}")
+print(f"Общий размер памяти: {total_size} байт")
+print(f"Время выполнения: {execution_time:.8f} секунд")
